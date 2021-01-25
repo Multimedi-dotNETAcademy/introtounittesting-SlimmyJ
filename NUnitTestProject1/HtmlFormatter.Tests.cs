a865b5650ca82d2cf3e1d8f36405176be3a54870
Simon Van Tittelboom <simon.van.tittelboom@gmail.com>
2021-01-25 13:33:13 +0100

    added HtmlFormatter via @Emma

---
+++ NUnitTestProject1/HtmlFormatter.Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.Tests
+{
+    public class HtmlFormatterTests
+    {
+        public HtmlFormatter testformatter = new HtmlFormatter();
+
+        [SetUp]
+        public void SetUp()
+        {
+            testformatter = new HtmlFormatter();
+        }
+
+        [TestCase("I am bold", "<strong>I am bold</strong>")]
+        [TestCase("I am bald", "<strong>I am bald</strong>")]
+        public void HtmlFormatter_WhenCalled_ReturnCode(string input, string expectedResult)
+        {
+            string result = testformatter.FormatAsBold(input);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
